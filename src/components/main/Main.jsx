baa760e8c8460ce88330cf88629a72b0eb6c3b6b
Hurkan Kalan <hurkan.kalan@gmail.com>
2021-11-05 10:27:00 +0100

    composant parking relais

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import ParkingRelais from './PR';
 
 function Main() {
-  return <div />;
+  return (
+    <div>
+      <ParkingRelais />
+    </div>
+  );
 }
 
 export default Main;
