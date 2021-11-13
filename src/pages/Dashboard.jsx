d58968ba8c7bddd7338aa7a10cba6c65abcdcf5f
FabriceSerapion <fabrice-serapion_student2021@wilder.school>
2021-11-13 15:46:42 +0100

    ajout de la user story infos trafic et ajout de deux imports manquants dans l'index.css

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
+import InfosTrafic from '../components/Infos-trafic';
 
 function Dashboard() {
   return (
     <div>
       <Navbar />
+      <InfosTrafic />
       <p>My dashboard</p>
     </div>
   );
